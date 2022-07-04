944b1840a537e4478f258a69908ddebad5ee4851
shagat <shagat.bing@gmail.com>
2022-07-04 12:05:07 +0530

    added a fix to the background not working.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,8 @@ import { bubbleData } from './data';
 export class AppComponent implements OnInit {
   title = 'algorithm-app';
   chartOptions = {
-    backgoundColor: "#F5DEB3",
+    backgroundColor: "#F5DEB3",
+    // theme: "dark1",
     title: {
       text: "Basic Column Chart in Angular"
     },
@@ -24,48 +25,12 @@ export class AppComponent implements OnInit {
         { label: "Mango",  y: 30  },
         { label: "Grape",  y: 28  }
       ]
-    }]
+    }
+  ]
 			  
   };
 
-  // bubbleData: any[] = [];
-  // view: [number, number] = [700,400];
-  // options
-  // showXAxis: boolean = true;
-  // showYAxis: boolean = true;
-  // gradient: boolean = false;
-  // showLegend: boolean = true;
-  // showXAxisLabel: boolean = true;
-  // yAxisLabel: string = 'Population';
-  // showYAxisLabel: boolean = true;
-  // xAxisLabel: string = 'Years';
-  // maxRadius: number = 20;
-  // minRadius: number = 5;
-  // xScaleMin: number = 70;
-  // xScaleMax: number = 70;
-  // yScaleMin: number = 70;
-  // yScaleMax: number = 85;
-  // colorScheme = '#5AA454';
-  // colorScheme = {
-  //   domain:
-  //    ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
-  // };
+  constructor() {}
 
-  constructor() {
-    // Object.assign(this, { bubbleData });
-  }
-  ngOnInit(): void {
-    // throw new Error('Method not implemented.');
-  }
-  // onSelect(data: any): void {
-  //   console.log('Item clicked', JSON.parse(JSON.stringify(data)));
-  // }
-
-  // onActivate(data: any): void {
-  //   console.log('Activate', JSON.parse(JSON.stringify(data)));
-  // }
-
-  // onDeactivate(data: any): void {
-  //   console.log('Deactivate', JSON.parse(JSON.stringify(data)));
-  // }
+  ngOnInit(): void {}
 }
