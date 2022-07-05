872b2a9c035b4a33235dea8b7cdc2a91414ff10d
shagat <shagat.bing@gmail.com>
2022-07-05 14:58:15 +0530

    added some buttons and routes to diffrent paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { SortStartComponent } from './start/sort-start/sort-start.component';
 const routes: Routes = [
   {path: '', component: HomeComponent, children: [
     {path:'sort-start', component: SortStartComponent},
-    {path:'sort-item', component: SortItemComponent}
+    {path:'sort-item', component: SortItemComponent},
   ]}
 ];
 
