e6af9f0bb1244410b906a1c3b45cb9209bc319dd
shagat <shagat.bing@gmail.com>
2022-07-06 22:02:40 +0530

    added slider in configuration

--- src/app/start/sort-start/sort-start.component.ts
+++ src/app/start/sort-start/sort-start.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-sort-start',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sort-start.component.css']
 })
 export class SortStartComponent implements OnInit {
-
+  @ViewChild('sortForm', { static: false }) sForm: NgForm;
+  inputValue = 1;
+  speedValue = 1;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log(this.inputValue);
   }
 
 }
