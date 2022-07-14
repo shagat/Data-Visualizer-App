13aed9e635c157b4fb7a4f912034cdda9ce1c03b
shagat <shagat.bing@gmail.com>
2022-07-14 22:22:08 +0530

    preview mode styling

--- src/app/visualizer/visualizer.component.ts
+++ src/app/visualizer/visualizer.component.ts
@@ -30,14 +30,32 @@ export class VisualizerComponent implements OnInit, OnDestroy {
     )
 
     this.chart = new Chart('canvas', {
-      type: 'line',
+      type: 'bar',
       data: {
         labels: this.previewIndexData,
         datasets: [
           {
             label: 'Value',
             data: this.inputData.input,
-            backgroundColor: ['rgb(13, 110, 253)'],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.5)',
+              'rgba(255, 159, 64, 0.5)',
+              'rgba(255, 205, 86, 0.5)',
+              'rgba(75, 192, 192, 0.5)',
+              'rgba(54, 162, 235, 0.5)',
+              'rgba(153, 102, 255, 0.5)',
+              'rgba(201, 203, 207, 0.5)'
+            ],
+            borderColor: [
+              'rgb(255, 99, 132)',
+              'rgb(255, 159, 64)',
+              'rgb(255, 205, 86)',
+              'rgb(75, 192, 192)',
+              'rgb(54, 162, 235)',
+              'rgb(153, 102, 255)',
+              'rgb(201, 203, 207)'
+            ],
+            borderWidth: 1,
           }
         ]
       },
@@ -52,6 +70,7 @@ export class VisualizerComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
+    this.chart.destroy();
     this.dataArraySub.unsubscribe();
   }
 }
