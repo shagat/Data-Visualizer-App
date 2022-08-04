8df65d3c92e2b8a25cff0fa786551cc63e46c6d2
shagat <shagat.bing@gmail.com>
2022-08-04 20:22:43 +0530

    added some fix to the UI

--- src/app/data-start/data.service.ts
+++ src/app/data-start/data.service.ts
@@ -14,25 +14,45 @@ export class DataService {
   dataSubject = new Subject<[Data, Data]>();
   url_api_gsdp =
     'https://api.data.gov.in/resource/adb4b1da-159f-46b3-a9c0-0545fe9ddda0?api-key=';
-  url_api_2 =
-    'https://api.data.gov.in/resource/a12ac9a7-99a9-4808-b987-42ed0223385e?api-key=';
+    url_api_2 =
+    'https://api.data.gov.in/resource/a5df75bc-4578-48ad-bc9d-e6eb4b63de0a?api-key=';
 
-  constructor(private httpClient: HttpClient) {}
+    constructor(private httpClient: HttpClient) {}
+
+    // fetchData() {
+    //   return this.httpClient
+    //     .get<{}>(this.url_api_2 + DATA_GOV_API, {
+    //       params: { format: 'json', limit: '35' },
+    //     })
+    //     .pipe(
+    //       map((obj) => {
+    //         this.resObj = obj;
+    //         return obj['records'];
+    //       }),
+    //       tap((obj) => {
+    //         obj.forEach((e: object) => {
+    //           this.resData.push(e);
+    //         });
+    //       }),
+    //       catchError((error) => {
+    //         throw new Error(error);
+    //       })
+    //     )
+    //     .subscribe();
+    // }
 
   fetchData() {
     return this.httpClient
-      .get<{}>(this.url_api_gsdp + DATA_GOV_API, {
-        params: { format: 'json', limit: '35' },
+      .get<{}>(this.url_api_2 + DATA_GOV_API, {
+        params: { format: 'json', limit: '50' },
       })
       .pipe(
         map((obj) => {
-          this.resObj = obj;
+          console.log(obj)
           return obj['records'];
         }),
         tap((obj) => {
-          obj.forEach((e: object) => {
-            this.resData.push(e);
-          });
+          console.log(obj)
         }),
         catchError((error) => {
           throw new Error(error);
@@ -50,12 +70,12 @@ export class DataService {
         this.data1.data.push(e[1]);
         this.data1.label.push(
           e[0].replace(/[a-zA-Z]+/g, '').replaceAll('_', ' ')
-        );
-      } else if (index > 0) {
-        this.data2.data.push(e[1]);
-        this.data2.label.push(
-          e[0].replace(/[a-zA-Z]+/g, '').replaceAll('_', ' ')
-        );
+          );
+        } else if (index > 0) {
+          this.data2.data.push(e[1]);
+          this.data2.label.push(
+            e[0].replace(/[a-zA-Z]+/g, '').replaceAll('_', ' ')
+            );
       }
     });
     // console.log(this.data); Raw data afer filter
@@ -67,8 +87,9 @@ export class DataService {
     this.resData.every((e) => {
       if (e['state_uts'].includes(index)) {
         let result = this.convertSingleJSONtoArray(e);
-        this.getLabelAndData(result);
-        this.dataSubject.next([this.data1, this.data2]);
+        console.log(result);
+        // this.getLabelAndData(result);
+        // this.dataSubject.next([this.data1, this.data2]);
         return false;
       }
       console.log('still going on');
